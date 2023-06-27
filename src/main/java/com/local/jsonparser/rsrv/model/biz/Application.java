3ca91b33b6525e72c3b53acddf0fd12b58d74b85
7ahyeon <7ahyeon@gmail.com>
2023-06-27 18:00:02 +0900

    #1 SendJsonCommand 수정중

--- src/main/java/com/local/jsonparser/rsrv/model/biz/Application.java
+++ src/main/java/com/local/jsonparser/rsrv/model/biz/Application.java
@@ -1,7 +1,16 @@
 package com.local.jsonparser.rsrv.model.biz;
 
+import com.local.jsonparser.rsrv.model.biz.cli.RsrvSelectService;
+import com.local.jsonparser.rsrv.model.biz.request.RequestService;
+
 public class Application {
     public static String application() {
-        return null;
+        AppConfig appConfig = new AppConfig();
+        RsrvSelectService rsrvSelectService = appConfig.rsrvSelectService();
+        RequestService requestService = appConfig.requestService();
+
+        int select = rsrvSelectService.serviceSelect();
+        String jsonContent = requestService.createRequestJson(select);
+        return jsonContent;
     }
 }
--- src/main/java/com/local/jsonparser/rsrv/model/command/SendJsonCommand.java
+++ src/main/java/com/local/jsonparser/rsrv/model/command/SendJsonCommand.java
@@ -1,7 +1,16 @@
 package com.local.jsonparser.rsrv.model.command;
 
+import com.local.jsonparser.rsrv.model.biz.Application;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -11,10 +20,51 @@ public class SendJsonCommand implements Command {
         try {
             // 요청을 보낼 URL
             URL url = new URL("http://localhost:8002/main.do");
-//            String SendRequest
+            String sendRequest = Application.application();
+            HttpURLConnection con = null;
+            con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.connect();
+
+            send(con, sendRequest.getBytes());
+            int resCode = con.getResponseCode();
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                String result = read(con);
+                ServletOutputStream out = res.getOutputStream();
+                out.println(result);
+            } else {
+                throw new IOException(
+                        "ERROR : Communication Error\nMSG Code : " + resCode);
+                )
+            } catch (Exception e){
+
+            }
+
         } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return null;
+    }
+    private void send(HttpURLConnection con, byte[] p_writeMsg) {
+        DataOutputStream dos = null;
+        try {
+            dos = new DataOutputStream(con.getOutputStream());
+            dos.write(p_writeMsg);
+            dos.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String read(HttpURLConnection con) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String strData = null;
+        StringBuffer sb = new StringBuffer();
+        while ((strData = br.readLine()) != null) {
+            sb.append(strData);
+        }
+        return new String(sb.toString().getBytes());
     }
 }
